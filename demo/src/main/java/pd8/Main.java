b0bda24b83dca7b1f2c10de845dc9f0e00208a61
iaravitabar <iara.vitabar@correo.ucu.edu.uy>
2025-03-24 16:37:48 -0300

    ejemplo de marcapasos

--- demo/src/main/java/pd8/Main.java
+++ demo/src/main/java/pd8/Main.java
@@ -3,6 +3,15 @@ package pd8;
 public class Main {
     public static void main(String[] args) {
 
+        Marcapasos marcapasos = new Marcapasos(
+            (short) 120, // Presión Sanguínea
+            (short) 80,  // Frecuencia Cardíaca
+            90,          // Nivel de Azúcar
+            1500000000L, // Máxima Fuerza
+            72.5f,       // Mínimo Tiempo entre Latidos
+            85.75,       // Batería Restante
+            "ABC12345"   // Código del Fabricante
+        );
         
         System.out.println("Consumo de memoria de un objeto Marcapasos:");
         System.out.println("Presión Sanguínea (short): 2 bytes");
